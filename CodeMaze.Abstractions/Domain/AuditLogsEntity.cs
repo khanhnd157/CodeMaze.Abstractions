64d0c8aaf4b8ffbcc117bc22f47a5e50a48917c9
Duy Khanh <45937431+khanhnd157@users.noreply.github.com>
2021-04-16 22:16:21 +0700

    Init project

---
+++ CodeMaze.Abstractions/Domain/AuditLogsEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CodeMaze.Abstractions.Domain
+{
+    public abstract class AuditLogsEntity : BaseEntity
+    {
+        public string CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
